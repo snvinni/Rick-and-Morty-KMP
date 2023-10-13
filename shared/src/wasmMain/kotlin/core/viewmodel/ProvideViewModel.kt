76f4771f11bdf765bd26de07d089f4a4c3379e8b
irineu silva <mmmirineusilva@gmail.com>
2023-10-13 00:56:28 -0300

    feat: persistent view model (wasm, desktop)

--- shared/src/wasmMain/kotlin/core/viewmodel/ProvideViewModel.kt
+++ shared/src/wasmMain/kotlin/core/viewmodel/ProvideViewModel.kt
@@ -1,19 +1,22 @@
 package core.viewmodel
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import feature.app.AppViewModel
 import feature.home.HomeViewModel
 
 actual object ProvideViewModel {
 
+    private val appViewModel by lazy { AppViewModel() }
+
+    private val homeViewModel by lazy { HomeViewModel() }
+
     @Composable
     actual fun provideAppViewModel(): AppViewModel {
-        return remember { AppViewModel() }
+        return appViewModel
     }
 
     @Composable
     actual fun provideHomeViewModel(): HomeViewModel {
-        return remember { HomeViewModel() }
+        return homeViewModel
     }
 }
